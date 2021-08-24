bec8766af544b89f5c27975fe51bb994088942ce
Jaylen Carroll <jaylencarroll87@gmail.com>
2021-08-23 23:06:35 -0400

    Pictures Attached Dice Roller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,8 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        // Do a dice roll when the app starts
+        rollDice()
     }
 
     /*
@@ -23,16 +28,31 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         //create a new Dice object with 6 sides and roll it
         val dice = Dice(6)
-        val dice2 = Dice(99)
         val diceRoll = dice.roll()
-        val diceRoll2 = dice2.roll()
+
 
         //Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-        resultTextView.text = diceRoll.toString()
-        resultTextView2.text = diceRoll2.toString()
+        //Usng ImageView in the Layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // Determine the source ID to use based on when the dice rolls
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        //Update the ImageView with the right source ID
+        diceImage.setImageResource(drawableResource)
+        /*This code calls the `setImageResource()` method on the `ImageView`,
+         passing the resource ID for the `dice_2` image. This will update the `ImageView`
+          on screen to display the `dice_2` image.
+         */
+        diceImage.contentDescription = diceRoll.toString()
     }
+
 
     class Dice(private val numSides: Int) {
         //This is an actual funtion that "rolls" a int between the numbers 1 and 6(numSides)
